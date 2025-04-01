c3c15205da5ff922a210aecafa47fdfbd85a9e38
Brianoct <brianoct@gmail.com>
2025-04-01 07:17:19 -0400

    Add new migration to force superuser creation

---
+++ moneyapp/migrations/0003_force_superuser_again.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+def force_superuser_again(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+    username = 'brian'
+    password = 'Brian2025'
+    email = 'brian@example.com'
+    # Delete any existing user to avoid duplicates
+    User.objects.filter(username=username).delete()
+    # Create the superuser
+    User.objects.create_superuser(username, email, password)
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),
+        ('moneyapp', '0002_create_superuser'),
+    ]
+
+    operations = [
+        migrations.RunPython(force_superuser_again),
+    ]
